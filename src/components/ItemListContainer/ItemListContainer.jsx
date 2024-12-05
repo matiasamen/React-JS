8fd73f49a4a2e3278aa6e6c89029abe19dca1a10
matiasamen <165975099+matiasamen@users.noreply.github.com>
2024-12-05 00:19:33 -0300

    Delete src directory

--- src/components/ItemListContainer/ItemListContainer.jsx
+++
@@ -1,8 +0,0 @@
-import { GridItem } from "@chakra-ui/react"
-
-const ItemListContainer = ({greeting}) => {
-return(
-    <div style={{fontSize:"2rem", fontWeight: "bold", height:"90vh", display:"flex",alignItems:"center", justifyContent:"center"}}>{greeting}</div>
-)
-}
-export default ItemListContainer
